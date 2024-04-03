ceaf53b601a68f7f0ff29a4684753ad8a0a3d796
Jess Bring-Larsen <jessbringlarsen@gmail.com>
2024-04-03 22:35:31 +0200

    Added server sent event

--- src/main/java/dk/bringlarsen/springkafkaexploration/QuotesResource.java
+++ src/main/java/dk/bringlarsen/springkafkaexploration/QuotesResource.java
@@ -1,21 +1,33 @@
 package dk.bringlarsen.springkafkaexploration;
 
+import dk.bringlarsen.springkafkaexploration.consumer.QuoteConsumer;
 import dk.bringlarsen.springkafkaexploration.producer.QuoteRequestProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@RestController()
+@RestController
 @RequestMapping(value = "/quotes")
 public class QuotesResource {
 
     @Autowired
     QuoteRequestProducer quoteRequestProducer;
+    @Autowired
+    QuoteConsumer quoteConsumer;
 
     @PostMapping(value = "/request", produces = MediaType.TEXT_PLAIN_VALUE)
     public String requestQuote() {
         return quoteRequestProducer.requestQuote();
+    }
+
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter getQuotes() {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        quoteConsumer.addEmitter(emitter);
+        return emitter;
     }
 }
--- src/main/java/dk/bringlarsen/springkafkaexploration/consumer/QuoteConsumer.java
+++ src/main/java/dk/bringlarsen/springkafkaexploration/consumer/QuoteConsumer.java
@@ -1,21 +1,23 @@
 package dk.bringlarsen.springkafkaexploration.consumer;
 
 import dk.bringlarsen.springkafkaexploration.Properties;
-import dk.bringlarsen.springkafkaexploration.processor.Quote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.util.Random;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class QuoteConsumer {
 
     private final Logger log = LoggerFactory.getLogger(QuoteConsumer.class);
+
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
     final Properties properties;
 
     @Autowired
@@ -23,8 +25,23 @@ public class QuoteConsumer {
         this.properties = properties;
     }
 
+    public void addEmitter(SseEmitter emitter) {
+        emitters.add(emitter);
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
+    }
+
+
     @KafkaListener(topics = "#{properties.getQuoteTopic}")
     void onQuote(String quote) {
         log.debug("Created quote={}", quote);
+        for (SseEmitter emitter : emitters) {
+            try {
+                emitter.send(quote);
+            } catch (IOException e) {
+                emitter.complete();
+                emitters.remove(emitter);
+            }
+        }
     }
 }
